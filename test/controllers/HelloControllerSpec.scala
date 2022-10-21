7d44580c360e2d16cab6aee11044193ffbb6c9be
Mashimo <mashita1023@gmail.com>
2022-10-22 03:05:24 +0900

    GET and POST

--- test/controllers/HelloControllerSpec.scala
+++ test/controllers/HelloControllerSpec.scala
@@ -2,6 +2,7 @@ package controllers
 
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
+import play.api.libs.json.Json
 import play.api.test._
 import play.api.test.Helpers._
 
@@ -13,6 +14,23 @@ class HelloControllerSpec extends PlaySpec with GuiceOneAppPerTest {
       val response = route(app, request).get
 
       status(response) mustBe OK
+      contentType(response) mustBe Some("text/plain")
+      contentAsString(response) mustBe "Hello World"
+    }
+  }
+
+  "HelloController POST" must {
+
+    "access /hello with POST method" in {
+      val request = FakeRequest(POST, "/hello")
+      val response = route(app, request).get
+
+      status(response) mustBe OK
+      contentType(response) mustBe Some("application/json")
+      contentAsString(response) mustBe Json.obj(
+        "hello"    -> "world",
+        "language" -> "scala"
+      )
     }
   }
 }
